d33b469be88493d4afaeec827f3a252819ac8d03
MichalMarchwinski <michal.marchwinski@gmail.com>
2018-07-29 14:54:15 +0200

    geters added and initialized method

--- src/main/java/com/sdacademy/javaFX/controler/PersonControler.java
+++ src/main/java/com/sdacademy/javaFX/controler/PersonControler.java
@@ -1,6 +1,37 @@
 package com.sdacademy.javaFX.controler;
 
+import com.sdacademy.javaFX.model.Person;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class PersonControler {
 
+    @FXML
+    private TableView<Person> personTable;
+    @FXML
+    private Label nameLabel;
+    @FXML
+    private Label lastnameLabel;
+    @FXML
+    private Label streetLable;
+    @FXML
+    private Label cityLable;
+    @FXML
+    private Label postalCodeLable;
+    @FXML
+    private Label telephoneLable;
+    @FXML
+    private TableColumn<Person, String> nameCol;
+    @FXML
+    private TableColumn<Person, String> lastnameCol;
+
+
+    @FXML
+    public void initialize(){
+        nameCol.setCellValueFactory(cell -> cell.getValue().nameProperty());
+        lastnameCol.setCellValueFactory(cell -> cell.getValue().lastnameProperty());
+    }
 
 }
--- src/main/java/com/sdacademy/javaFX/model/Person.java
+++ src/main/java/com/sdacademy/javaFX/model/Person.java
@@ -20,5 +20,51 @@ public class Person {
         this.lastname = new SimpleStringProperty(lastname);
     }
 
+    public String getName() {
+        return name.get();
+    }
 
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public String getLastname() {
+        return lastname.get();
+    }
+
+    public StringProperty lastnameProperty() {
+        return lastname;
+    }
+
+    public String getStreet() {
+        return street.get();
+    }
+
+    public StringProperty streetProperty() {
+        return street;
+    }
+
+    public String getCity() {
+        return city.get();
+    }
+
+    public StringProperty cityProperty() {
+        return city;
+    }
+
+    public String getPostalCode() {
+        return postalCode.get();
+    }
+
+    public StringProperty postalCodeProperty() {
+        return postalCode;
+    }
+
+    public String getTelephone() {
+        return telephone.get();
+    }
+
+    public StringProperty telephoneProperty() {
+        return telephone;
+    }
 }
